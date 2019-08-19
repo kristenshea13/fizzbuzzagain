34e0322506357a09bb6951963fa48125f9a5ed56
Kristen Shea <kstar61@live.com>
2019-08-18 21:32:27 -0500

    ,

--- fizzbuzzagain/Program.cs
+++ fizzbuzzagain/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace fizzbuzzagain
 {
@@ -6,7 +7,46 @@ namespace fizzbuzzagain
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+            //print fizz if number is divisible by 3
+            //print buzz if number is divisible by 5
+            //print fizzbuzz if number is divisible by 3 and 5
+
+
+            List<int> list = new List<int>();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                list.Add(i);
+
+            }
+
+            foreach (int number in list)
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine($"Fizzbuzz");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine($"Fizz");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine($"Buzz");
+                }
+                else
+                {
+                    Console.WriteLine($"{number}");
+                }
+
+            }
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
